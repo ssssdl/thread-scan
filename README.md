b7b318c3973b15c5a3cd7c0a275fd38e533cac9b
ssssdl@qq.com <1463620240@qq.com>
2019-04-03 20:04:00 +0800

    添加代理功能

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 ### 开始重新组织一下代码 命令结构如下
 ![命令结构](https://i.loli.net/2019/04/03/5ca484191e4c3.png)
+
+### 代理使用的[IPProxyPool](https://github.com/qiyeboy/IPProxyPool)获取代理ip和端口
+> 简易搭建方式
+```
+docker run -d -t --restart=always --privileged --name Iproxypool -p 8000:8000 jackadam/ipproxypool
+```
--- webscan.py
+++ webscan.py
@@ -6,12 +6,16 @@ import socket
 import nmap
 import time
 import threading
+import json
 from queue import Queue
 
 # 核心类
 class Scan(object):
     ok_code = [200,403,302]
     q = Queue()
+    GET_PROXY = ''
+    def __init__(self,get_proxy=''):
+        self.GET_PROXY = get_proxy
     # 基础tcp端口扫描 
     def portScan(self,ip,port,timeout = 0.01):
         try:
@@ -40,10 +44,10 @@ class Scan(object):
             for port in lport:
                 print('port : %s \tstate : %s' %(port,np[ip][proto][port]['state'])) 
     # 基础目录扫描
-    def indexScan(self,url):
+    def indexScan(self,url,proxies = ''):
         try:
             s = requests.session()
-            r = s.put(url)
+            r = s.put(url,proxies=proxies)
             if r.status_code in self.ok_code:
                 print(str(r.status_code)+" : "+url)
         except:
@@ -77,24 +81,51 @@ class Scan(object):
             threads[i].start()
         self.q.join()
     # 目录扫描 根据字典大小耗时不等 相对耗时较长 要用多线程分配字典 24kb的字典扫了165s
-    def indexScancommon(self,url):
+    def indexScancommon(self,url,proxy=False):
         # 如果出现‘gbk' codec can't decode bytes in position 31023: illegal multibyte sequence 
         # fileIndex = open('./dirScan/PHP.txt','r',encoding='gb18030',errors='ignore')
         fileIndex = open('./dirScan/DIR.txt','r')
         for line in fileIndex.readlines():
-            # 这里还有一个换行符的问题，一开始怎么也扫不到  发现合成的url结尾有换行
             line=line.strip('\n')
-            self.indexScan(url+'/'+line)
+            url = url+'/'+line
+            proxies = ''
+            if proxy:
+                #获取代理
+                proxies = self.__getProxyIp()
+            self.indexScan(url,proxies)
+
+    # 获取代理
+    def __getProxyIp(self,types='2',count='1',country='国内'):
+        try:
+            r = requests.get(self.GET_PROXY+'?types='+types+'&count='+count+'&country='+country)
+            print(self.GET_PROXY+'?types='+types+'&count='+count+'&country='+country,r.text)
+            ip_ports = json.loads(r.text)
+            ip = ip_ports[0][0]
+            port = ip_ports[0][1]
+            proxies={
+                'http':'http://%s:%s'%(ip,port),
+                'https':'http://%s:%s'%(ip,port)
+            }
+        except (IndexError,json.decoder.JSONDecodeError):
+            print('获取代理IP失败，程序退出！！')
+            sys.exit()
+        except requests.exceptions.ConnectionError:
+            print('代理接口拒绝连接，程序退出！！')
+            sys.exit()
+        return proxies
+        
 
 
 def main(argv):
-    IP = ''
-    HOST = ''
-    URL = ''
-    SCAN = Scan()
+    IP = ''         # 要端口扫描的站点ip
+    HOST = ''       # 要目录扫描的站点域名
+    URL = ''        # 要目录扫描的url
+    PROXY = False   # 是否使用代理
+    GET_PROXY = 'http://45.76.101.190:8000/'  # 填写已经搭建好的获取代理的IPProxyPool接口 不使用代理可以不写 如http://127.0.0.1:8000/(结尾有斜杠)
+    SCAN = Scan(GET_PROXY)
     #获取命令行参数
     try:
-        opts,args = getopt.getopt(argv,"h:u:",["version","help","url="])
+        opts,args = getopt.getopt(argv,"h:u:",["version","help","proxy","url="])
     except getopt.GetoptError:
         # 打印help
         sys.exit(2)
@@ -104,13 +135,16 @@ def main(argv):
             sys.exit()
         elif opt == "--version":
             print(r'webscan version 1.0 ( http://github.com/ssssdl/thread-scan )')
+            sys.exit()
         elif opt in ("-u","--url"):
             URL = arg
+        elif opt == "--proxy":
+            PROXY = True
     # 执行端口扫描
 
     # 执行目录扫描
     if URL != '':
-        SCAN.indexScancommon(URL)
+        SCAN.indexScancommon(URL,PROXY)
         
 
 if __name__=='__main__':
